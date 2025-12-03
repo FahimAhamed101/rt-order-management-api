c51675fd6282a661a43f25446d88fa267801afad
fahim <fahimahamedweb@gmail.com>
2025-12-04 03:04:58 +0600

    authentciation api  done using passport

--- config/auth.php
+++ config/auth.php
@@ -40,6 +40,11 @@ return [
             'driver' => 'session',
             'provider' => 'users',
         ],
+      
+      'api' => [
+        'driver' => 'passport', // Change from 'token' to 'passport'
+        'provider' => 'users',
+    ],
     ],
 
     /*
